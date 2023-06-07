a25c5a2b02325e27da3c2347e35f5fba9e6e1ba0
Domjon24 <Domjon24@gmail.com>
2023-06-07 02:59:23 -0500

    created enemy placeholder, it now moves horizonally across map. video stopped at 1:03:54. https://www.youtube.com/watch?v=C4_iRLlPNFc

--- js/index.js
+++ js/index.js
@@ -5,7 +5,22 @@ canvas.height = "768"
 c.fillStyle = 'white'
 c.fillRect(0, 0, canvas.width, canvas.height)
 const image = new Image()
-image.onload = () => {
-    c.drawImage(image, 0, 0)
+
+
+image.onload = () => { 
+    
+    animate()
 }
+
 image.src = 'img/Tower Defense Map.png'
+
+let x = 42;
+/* animation */
+function animate() {
+    requestAnimationFrame (animate)
+
+    c.drawImage(image, 0, 0)
+    c.fillStyle = 'purple'
+    c.fillRect(x, 320, 75, 75)
+    x++
+}
