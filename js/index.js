7c0176e23126f351f6d7290e2bbab4843c0d2f48
Domjon24 <Domjon24@gmail.com>
2023-06-08 14:11:20 -0500

    created enemy constructor for enemy class. made 2nd enemy object

--- js/index.js
+++ js/index.js
@@ -1,3 +1,4 @@
+// variables initialized before they're called
 const canvas = document.querySelector('canvas')
 const c = canvas.getContext('2d')
 canvas.width = "1280"
@@ -5,23 +6,35 @@ canvas.height = "768"
 c.fillStyle = 'white'
 c.fillRect(0, 0, canvas.width, canvas.height)
 const image = new Image()
-
-
 image.onload = () => { 
-    
     animate()
 }
-
 image.src = 'img/Tower Defense Map.png'
 
-let x = 42;
+// enemy class, with all nessecary properties
+class Enemy {
+    constructor({position = { x: 0, y: 0} }) {
+        this.position = position
+        this.width = 75
+        this.height = 75
+    }
+    draw() {
+        c.fillStyle = 'purple'
+        c.fillRect(this.position.x, this.position.y, this.width, this.height)
+    }
+    update() {
+        this.draw()
+        this.position.x += 1
+    }
+}
+
 /* animation */
+const enemy = new Enemy({position: {x: 42, y: 320}});
+const enemy2 = new Enemy({position: {x: -50, y: 320}});
+
 function animate() {
     requestAnimationFrame (animate)
-
     c.drawImage(image, 0, 0)
-    c.fillStyle = 'purple'
-    c.fillRect(x, 320, 75, 75)
-    x++
+    enemy.update()
+    enemy2.update()
 }
-// test
